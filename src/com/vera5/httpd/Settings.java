a49dfa016210ae929a84503a67010a5601d0091e
Nick Kolev <nickkolev@yahoo.com>
2014-12-18 09:59:39 +0100

    Restored latest state from a backup tar

---
+++ src/com/vera5/httpd/Settings.java
@@ -0,0 +1,55 @@
+package com.vera5.httpd;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class Settings extends PreferenceActivity {
+
+  OnSharedPreferenceChangeListener listener;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+		listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+			public void onSharedPreferenceChanged(SharedPreferences p, String key) {
+				setSummary(p, key);
+				// TODO Restart server if port changed
+			}
+		};
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(this);
+		setSummary(p, "port");
+		setSummary(p, "doc_root");
+		// More settings initialization
+		p.registerOnSharedPreferenceChangeListener(listener);
+	}
+
+	@Override
+	protected void onPause() {
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
+		super.onPause();
+	}
+
+	private void setSummary(SharedPreferences p, String key) {
+		try {
+			String value = p.getString(key, "").replaceAll("/$", "");
+			EditTextPreference pref = (EditTextPreference) findPreference(key);
+			if (key.equals("doc_root") && value.equals(""))
+				value = StartActivity.getDefaultDocRoot();
+			pref.setSummary(value);
+		} catch (Exception e) {
+			Log.e("httpd.setSummary()", e.getMessage());
+		}
+	}
+
+}
