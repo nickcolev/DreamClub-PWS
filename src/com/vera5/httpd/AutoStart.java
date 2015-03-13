6a0d38171a57f78776747a4d892643ec2e3c25f0
Nick Kolev <nickkolev@yahoo.com>
2015-03-13 12:57:39 +0100

    AutoStart * Implemented logic to auto start depending on 'auto_start' preference

--- src/com/vera5/httpd/AutoStart.java
+++ src/com/vera5/httpd/AutoStart.java
@@ -3,12 +3,15 @@ package com.vera5.httpd;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class AutoStart extends BroadcastReceiver {
 
 	public void onReceive(Context arg0, Intent arg1) {
-		// FIXME if auto_start is false don't auto start
-		Intent intent = new Intent(arg0, ServerService.class);
-		arg0.startService(intent);
+		if (PreferenceManager.getDefaultSharedPreferences(arg0).getBoolean("auto_start", false)) {
+			Intent intent = new Intent(arg0, ServerService.class);
+			arg0.startService(intent);
+		}
 	}
 }
