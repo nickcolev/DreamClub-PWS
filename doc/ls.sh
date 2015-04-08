7908e27fb987f4519d201ec6c1b25ebbf5c474f6
Nick Kolev <nickkolev@yahoo.com>
2015-04-08 10:07:09 +0200

    Moved the code to 'code' folder

--- doc/ls.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Content-Type: text/plain"
-echo
-# How about /system/bin/ls -l $QUERY_STRING
-/system/bin/ls -l "$DOCUMENT_ROOT/$QUERY_STRING"
--- doc/screencap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Content-Type: text/plain"
-echo
-/system/bin/screenshot "$DOCUMENT_ROOT/tmp/shot2.png"
-echo "Done?"
-echo
